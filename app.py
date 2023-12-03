a1cd10d4e877866ba83a68a31f2da4d4b5a732a8
Matesoidicek <mat.dusek@gmail.com>
2023-12-03 17:23:34 +0100

    Completely reworking the game for better scene system, currently there is less functions than there were before

--- app.py
+++ app.py
@@ -1,146 +1,107 @@
-import pygame, sys
-import ButtonMaker
+import pygame
+import sys, Button
 
-pygame.init()
-width, height = 1900, 1000
-screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
+# Global variables
+width, height = 1280, 720
+FPS = 60
 
 
-#       FONT
-font = pygame.font.Font(None, 36)
+# ---------------------------------------------------------------- Game ----------------------------------------------------------------
+class Game:
+    def __init__(self):
 
-# ---------------------------------------------------------------- GAME SCENE ----------------------------------------------------------------
+        pygame.init()
+        self.screen = pygame.display.set_mode((width, height))
+        self.clock = pygame.time.Clock()
 
-def game():
-    running_game = True
+        self.GameStateManager = GameStateManager('start')
+        self.start = Start(self.screen, self.GameStateManager)
+        self.level = Level(self.screen, self.GameStateManager)
+
+        self.states = {'start':self.start, 'level':self.level}
+
+    def run(self):
+        while True:
+            # quit
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.states[self.GameStateManager.get_state()].run()
+            
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+# ---------------------------------------------------------------- Level ----------------------------------------------------------------
+
+
+class Level:
+    # Load player (sled)
+    sled = pygame.image.load("img/sled")
+
+    def __init__(self, screen, gameStateManager):
+        self.screen = screen
+        self.gameStateManager = gameStateManager
     
-    # Loading sled image
-    sled_old = pygame.image.load('img/snow_sled.png')
-    sled_new = pygame.transform.scale(sled_old, (80,80))
-    sled_rect = sled_new.get_rect()
-
-    # Player variables
-
-    player_x, player_y = width // 2, height // 2
-    player_speed = 0
-    max_speed = 5
-    acceleration = 0.1
-    deceleration = 0.2
-    rotation_speed = 5
+    def run(self):
+        
+        # Background
+        self.screen.fill((0,183,255))
 
 
-# ---------------------------------------------------------------- MAIN GAME LOOP ----------------------------------------------------------------
 
-    while running_game:
+
+
+
+# ---------------------------------------------------------------- Menu ----------------------------------------------------------------
+
+
+class Start:
+    
+    def __init__(self, screen, gameStateManager):
+        self.screen = screen
+        self.gameStateManager = gameStateManager
+    
+    def run(self):
 
         # Background
+        bg_image = pygame.image.load("img/bg.png")
+        self.screen.blit(bg_image, (0,0))
 
-        screen.fill((0,205,255))
-        
-    # Player loading
+        # Load button images
+        play_image = pygame.image.load("img/btn_play.png")
+        options_image = pygame.image.load("img/btn_options.png")
+        exit_image = pygame.image.load("img/btn_exit.png")
 
+        # Create buttons
+        play_btn = Button.Button(width // 2 - 137.5, height // 4, play_image, 0.5)
+        options_btn = Button.Button(width // 2 - 137.5, height // 2.5, options_image, 0.5)
+        exit_btn = Button.Button(width // 2 - 137.5, height // 1.8, exit_image, 0.5)
 
-        # Controlling the sled
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player_y -= player_speed
-        if keys[pygame.K_s]:
-            player_y += player_speed
-        if keys[pygame.K_a]:
-            player_x -= player_speed
-        if keys[pygame.K_d]:
-            player_x += player_speed
-
-        
-
-        # Calculate fps
-        fps = clock.get_fps()
-        # Render FPS text
-        fps_text = font.render(f"FPS: {int(fps)}", True, "Black")
-        screen.blit(fps_text, (10,10))
-
-        clock.tick(60)
-        print(clock)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running_game = False
-                pygame.quit()
-                sys.exit()
-                
-        pygame.display.update()
-
-# ---------------------------------------------------------------- MAIN MENU SCENE ----------------------------------------------------------------
-
-
-def main_menu():
-    #       FONT
-
-            # Buttons
-
-    # Load button images
-    play_img = pygame.image.load('img/btn_play.png').convert_alpha()
-    options_img = pygame.image.load('img/btn_options.png').convert_alpha()
-    exit_img = pygame.image.load('img/btn_exit.png').convert_alpha()
-
-    # Create buttons
-
-    play_btn = ButtonMaker.Button(675, 310, play_img, 1)
-    options_btn = ButtonMaker.Button(675, 495, options_img, 1)
-    exit_btn = ButtonMaker.Button(675, 680, exit_img, 1)
-
-
-    running_menu = True
-    background = pygame.image.load('img/bg.png')
-
-
-# ---------------------------------------------------------------- MAIN MENU LOOP ----------------------------------------------------------------
-
-    while running_menu:
-
-        screen.blit(background, (0,0))
-        pygame.display.set_caption('Main Menu')
-
-
-
-        # Drawing buttons
-
-        if play_btn.draw(screen):
-            running_menu = False
-            game()
-        if options_btn.draw(screen):
-            running_menu = False
-            print("options")
-        if exit_btn.draw(screen):
-            running_menu = False
+        if play_btn.draw(self.screen):
+            self.gameStateManager.set_state('level')
+        if options_btn.draw(self.screen):
+            pass
+        if exit_btn.draw(self.screen):
             pygame.quit()
             sys.exit()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running_menu = False
-                pygame.quit()
-                sys.exit()
-
-        # Calculate fps
-        fps = clock.get_fps()
-        # Render FPS text
-        fps_text = font.render(f"FPS: {int(fps)}", True, "Black")
-        screen.blit(fps_text, (10,10))
 
 
 
+# ---------------------------------------------------------------- GameStateManager ----------------------------------------------------------------
 
-        pygame.display.update()
+class GameStateManager:
+    def __init__(self, currentState):
+        self.currentState = currentState
         
-        print("MAIN MENU")
-        # CLOCK
-        clock.tick(25)
+    def get_state(self):
+        return self.currentState
+    
+    def set_state(self, state):
+        self.currentState = state
 
-main_menu()
-
-# GAME
-
-
-
+if __name__ == "__main__":
+    game = Game()
+    game.run()
